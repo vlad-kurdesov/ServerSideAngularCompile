92ea618c99d0ea2e6686227b15f7a83f38958ae6
Vlad Kurdesov <vkurdesov@marketvision.com>
2017-04-22 12:08:58 -0500

    added initial implementation for service & test client

---
+++ compiler.js
@@ -0,0 +1,71 @@
+var template = '<div ng-repeat="item in collection">{{item}}</div><br/>single value: {{testValue1}}';
+var templateScope = '{"testValue1":1,"collection":[1,2,3]}';
+
+var express = require('express')
+var app = express()
+const Browser = require('zombie');
+var fs = require('fs');
+var htmlmin = require('htmlmin');
+var bodyParser = require('body-parser')
+var open = require('open');
+
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
+}
+
+var contentsTemplate = fs.readFileSync('index.html', 'ascii');
+var indexPage = fs.readFileSync('testClient.html', 'ascii');
+
+var tempDir = __dirname;
+
+app.get('/', function(req, res){
+  res.send(indexPage);
+})
+
+app.post('/compile', function (req, res) {
+  var start = Date.now();
+  var tempFileName = (guid()+guid()).replace(/-/g,'')+".html";
+  var tempFilePath = tempDir + "\\"+tempFileName;
+  var contents = contentsTemplate;
+  contents = contents.replace("%text%",req.body.template);
+  contents = contents.replace("%variable%",req.body.variable);
+  fs.writeFileSync(tempFilePath, contents);
+
+  var browser = new Browser();
+  browser.visit('file://'+tempFilePath, function(a){
+    var compiledTemplate = browser.html('body');
+
+    var index = compiledTemplate.indexOf('>');
+    compiledTemplate = compiledTemplate.substr(index+2,compiledTemplate.length-index-11);
+
+    //clean up any angular specific code after binding
+    compiledTemplate = compiledTemplate
+      .replace(/(\ )?ng\-binding(\ )?/g,' ')
+      .replace(/(\ )?ng\-scope(\ )?/g,' ')
+      .replace(/(\ )?ng\-repeat=\"[a-zA-Z0-9\(\)\ ]+(\ )?\"/g,' ');
+    
+    res.send(JSON.stringify({
+      result: htmlmin(compiledTemplate),
+      time: (Date.now()-start)
+    }));
+
+    fs.unlink(tempFilePath);
+  });
+})
+
+app.listen(3000, function () {
+  console.log('Server launched on port 3000!')
+})
+
+open('http://localhost:3000');
